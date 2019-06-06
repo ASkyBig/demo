cbb90b064dd62891e435d751d2126c1dfadd21c2
p_jianjyang <p_jianjyang@tencent.com>
2019-06-06 21:30:32 +0800

    simple-comment

--- React/simple-comment/service-worker.js
+++ React/simple-comment/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hello-react/precache-manifest.296a8787a7956a05452961cb50820eee.js"
+  "/precache-manifest.49007bbdb0275e428f0872b039b85c81.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hello-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
